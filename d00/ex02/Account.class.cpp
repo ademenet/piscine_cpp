9f8a874cdffdddaad407a2aa01ce9ce32db50836
Alain <ademenet@scortex.fr>
2017-03-31 09:38:27 +0200

    Finish, make a diff with log: it's the same

--- d00/ex02/Account.class.cpp
+++ d00/ex02/Account.class.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 #include "Account.class.hpp"
 
-// TODO Decomposer le hpp
-// TODO Coder les methodes et tout et tout
-
+// Those getters return the static values, which are totals of accounts, amount,
+// and so on.
 int Account::getNbAccounts(void)
 {
 	return(_nbAccounts);
@@ -24,15 +23,18 @@ int Account::getNbWithdrawals(void)
 	return(_totalNbWithdrawals);
 }
 
+// This function is called regurlarly just before deposits and withdrawals
+// operations.
 void Account::displayAccountsInfos(void)
 {
-	std::cout << "Index: " << _nbAccounts << std::endl;
-	std::cout << "Total amount: " << _totalAmount << std::endl;
-	std::cout << "Total number of deposits: " << _totalNbDeposits << std::endl;
-	std::cout << "Total number of withdrawals: " << _totalNbWithdrawals << std::endl;
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount;
+	std::cout << ";deposits:" << _totalNbDeposits << ";withdrawals:";
+	std::cout << _totalNbWithdrawals << std::endl;
 	return;
 }
 
+// Opening a new account.
 Account::Account(int initial_deposit)
 {
 	this->_amount = initial_deposit;
@@ -43,45 +45,54 @@ Account::Account(int initial_deposit)
 	this->_nbCheckAmount = 0;
 
 	Account::_displayTimestamp();
-	std::cout << "index:" << _nbAccounts++ << ";amount:" << initial_deposit << ";created" << std::endl;
+	std::cout << "index:" << _nbAccounts++ << ";amount:" << initial_deposit;
+	std::cout << ";created" << std::endl;
 	return;
 }
 
+// Closing an account.
 Account::~Account(void)
 {
 	Account::_displayTimestamp();
 	_nbAccounts--;
-	std::cout << "index:" << _nbAccounts << ";amount:" << this->_amount << ";closed";
-	if (_nbAccounts > 0) std::cout << std::endl;
+	std::cout << "index:" << _nbAccounts << ";amount:" << this->_amount;
+	std::cout << ";closed" << std::endl;
+	// TODO check si retour a la ligne ou pas dans fichier
 	return;
 }
 
+// Deposit operation.
 void Account::makeDeposit(int deposit)
 {
 	if (deposit >= 0)
 	{
 		Account::_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+		std::cout << "index:" << this->_accountIndex << ";p_amount:";
+		std::cout << this->_amount << ";deposit:" << deposit;
 		this->_amount += deposit;
 		_totalAmount += deposit;
 		this->_nbDeposits++;
-		Account::_totalNbDeposits++;
-		std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
+		_totalNbDeposits++;
+		std::cout << ";amount:" << this->_amount << ";nb_deposits:";
+		std::cout << this->_nbDeposits << std::endl;
 	}
 	return;
 }
 
+// Withdrawal operation.
 bool Account::makeWithdrawal(int withdrawal)
 {
 	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:";
+	std::cout << "index:" << this->_accountIndex << ";p_amount:";
+	std::cout << this->_amount << ";withdrawal:";
 	if (withdrawal <= this->_amount)
 	{
 		this->_amount -= withdrawal;
 		_totalAmount -= withdrawal;
 		this->_nbWithdrawals++;
 		_totalNbWithdrawals++;
-		std::cout << withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		std::cout << withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:";
+		std::cout << this->_nbWithdrawals << std::endl;
 		return (true);
 	}
 	std::cout << "refused" << std::endl;
@@ -96,22 +107,27 @@ int Account::checkAmount(void) const
 
 void Account::displayStatus(void) const
 {
-	time_t	current_time = time(0);
-	struct	tm*local_t = localtime(&current_time);
-	char	time_dis[18];
-
-	strftime(time_dis, 18, "[%Y%d%m_%H%M%S] ", local_t);
-	std::cout << time_dis;
-	return;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount;
+	std::cout << ";deposits:" << this->_nbDeposits << ";withdrawals:";
+	std::cout << this->_nbWithdrawals << std::endl;
 }
 
-int _nbAccounts			= 0;
-int _totalNbDeposits	= 0;
-int _totalNbWithdrawals = 0;
+// Initialize the static attributes, it means that they are class members.
+int Account::_nbAccounts			= 0;
+int Account::_totalNbDeposits		= 0;
+int Account::_totalNbWithdrawals	= 0;
+int Account::_totalAmount			= 0;
 
 void Account::_displayTimestamp(void)
 {
+	time_t	current_time = time(0);
+	struct	tm*local_t = localtime(&current_time);
+	char	time_dis[32];
 
+	strftime(time_dis, 32, "[%Y%d%m_%H%M%S] ", local_t);
+	std::cout << time_dis;
+	return;
 }
 
 Account::Account(void)
