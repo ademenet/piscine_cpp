5ac3e7cabae9095c3029742174f44e6d32b47908
John Doe <ademenet@student.42.fr>
2017-04-05 13:59:35 +0200

    ex10 done

---
+++ d01/ex10/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+// TODO cat: test: Is a directory
+
+int main(int argc, char const *argv[]) {
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			std::ifstream	in(argv[i]);
+			std::string		line;
+			if (in.fail()) {
+				std::cerr << "cato9tails: " << argv[i] << ": "  << strerror(errno) << std::endl;
+				exit(0);
+			}
+			while(getline(in, line))
+				std::cout << line << std::endl;
+			in.close();
+		}
+	}
+	else {
+		std::string		line;
+		while (getline(std::cin, line)) {
+			std::cout << line << std::endl;
+		}
+	}
+	return 0;
+}
