d0640bf4b1b2a7c78aa3afaf52b4d1fb2a27c2ae
John Doe <ademenet@student.42.fr>
2017-04-05 21:18:24 +0200

    ex01 work in progress

---
+++ d02/ex01/Fixed.cpp
@@ -0,0 +1,56 @@
+#include "Fixed.hpp"
+
+const int Fixed::_fbits = 8;
+
+Fixed::Fixed(void) : _fpvalue(0) {
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+Fixed::Fixed(int nb) {
+	std::cout << "Integer constructor called" << std::endl;
+	this->_fpvalue = nb << (Fixed::_fbits);
+	return;
+}
+
+Fixed::Fixed(float nb) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_fpvalue = (int)roundf(nb * 256);
+	return;
+}
+
+Fixed::~Fixed(void){
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+Fixed::Fixed(Fixed const & src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs) {
+	std::cout << "Assignation operator called" << std::endl;
+	if (this != &rhs)
+		this->_fpvalue = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return(this->_fpvalue);
+}
+
+void Fixed::setRawBits(const int raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fpvalue = raw;
+}
+
+float toFloat(void) {
+	return(42.42f);
+}
+
+int toInt(void) {
+	return(1);
+}
