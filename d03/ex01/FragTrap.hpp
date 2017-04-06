973a22a822c7303f5bd2b49051c6becc2736f38d
John Doe <ademenet@student.42.fr>
2017-04-06 16:07:08 +0200

    ex00 use maxHitPoints instead of raw value, and init ex01

---
+++ d03/ex01/FragTrap.hpp
@@ -0,0 +1,42 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <iostream>
+
+class FragTrap {
+
+public:
+	FragTrap(std::string name);
+	FragTrap(FragTrap const &src);
+	~FragTrap(void);
+	FragTrap &operator=(FragTrap const &);
+
+	void rangedAttack(std::string const &target);
+	void meleeAttack(std::string const &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	void vaulthunter_dot_exe(std::string const &target);
+
+	void spoonAttack(std::string const &target);
+	void fartAttack(std::string const &target);
+	void stabiloAttack(std::string const &target);
+	void sisterAttack(std::string const &target);
+	void windAttack(std::string const &target);
+
+
+private:
+	FragTrap(void);
+
+	std::string			_name;
+	int					_hitpoints;
+	unsigned int		_maxhitpoints;
+	int					_energypoints;
+	unsigned int		_maxenergypoints;
+	unsigned int		_level;
+	unsigned int		_meleedamage;
+	unsigned int		_rangeddamage;
+	unsigned int		_armordamagereduction;
+
+};
+#endif
---
+++ d03/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include "FragTrap.hpp"
+
+int main(void) {
+	FragTrap	robot("Jack");
+
+	robot.rangedAttack("enemy");
+	robot.meleeAttack("enemy");
+	robot.takeDamage(30);
+	robot.takeDamage(100);
+	robot.beRepaired(40);
+	robot.beRepaired(100);
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.meleeAttack("enemy");
+	return 0;
+}
