2955c955087f9fc3394ac10e71b399b3015834a6
John Doe <ademenet@student.42.fr>
2017-04-07 20:19:49 +0200

    Test main

--- d03/ex03/main.cpp
+++ d03/ex03/main.cpp
@@ -7,10 +7,14 @@ int main(void) {
 	FragTrap	frag("Jack");
 	ScavTrap	scav("Jenny");
 
-	frag.rangedAttack("enemy");
+	std::cout << std::endl << "-- Tests for FragTrap" << std::endl;
 	frag.meleeAttack("enemy");
+	frag.rangedAttack("enemy");
 	frag.takeDamage(30);
-	frag.takeDamage(100);
+	frag.takeDamage(30);
+	frag.takeDamage(30);
+	frag.takeDamage(30);
+	frag.takeDamage(30);
 	frag.beRepaired(40);
 	frag.beRepaired(100);
 	frag.vaulthunter_dot_exe("enemy");
@@ -20,10 +24,14 @@ int main(void) {
 	frag.vaulthunter_dot_exe("enemy");
 	frag.meleeAttack("enemy");
 
-	scav.rangedAttack("enemy");
+	std::cout << std::endl << "-- Tests for ScavTrap" << std::endl;
 	scav.meleeAttack("enemy");
+	scav.rangedAttack("enemy");
 	scav.takeDamage(30);
-	scav.takeDamage(100);
+	scav.takeDamage(30);
+	scav.takeDamage(30);
+	scav.takeDamage(30);
+	scav.takeDamage(30);
 	scav.beRepaired(40);
 	scav.beRepaired(100);
 	scav.challengeNewcomer();
@@ -33,11 +41,20 @@ int main(void) {
 	scav.challengeNewcomer();
 	scav.meleeAttack("enemy");
 
+	std::cout << std::endl << "-- Instantiate ClapTrap" << std::endl;
 	ClapTrap	clap("John");
+
+	std::cout << std::endl << "-- Instantiate NinjaTrap" << std::endl;
 	NinjaTrap	ninja("Henry");
+
+	std::cout << std::endl << "-- Tests for NinjaTrap" << std::endl;
+	std::cout << "--- For clap == John" << std::endl;
 	ninja.ninjaShoebox(clap);
+	std::cout << "--- For frag == Jack" << std::endl;
 	ninja.ninjaShoebox(frag);
+	std::cout << "--- For scav == Jenny" << std::endl;
 	ninja.ninjaShoebox(scav);
+	std::cout << "--- For ninja == Henry" << std::endl;
 	ninja.ninjaShoebox(ninja);
 	return 0;
 }
