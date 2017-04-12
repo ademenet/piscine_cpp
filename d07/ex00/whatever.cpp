f2a4a49499e97836f2ff1d336d9486acb0b8b11f
John Doe <ademenet@student.42.fr>
2017-04-12 16:40:16 +0200

    Better main

--- d07/ex00/whatever.cpp
+++ d07/ex00/whatever.cpp
@@ -2,19 +2,21 @@
  * @Author: ademenet
  * @Date:   2017-04-12T13:20:56+02:00
  * @Last modified by:   ademenet
- * @Last modified time: 2017-04-12T14:00:26+02:00
+ * @Last modified time: 2017-04-12T16:38:56+02:00
  */
 
 #include <iostream>
 
-template<typename Type> void swap(Type &a, Type &b) {
+template<typename Type>
+void swap(Type &a, Type &b) {
 	Type tmp;
 	tmp = a;
 	a = b;
 	b = tmp;
 }
 
-template<typename Type> Type min(Type first, Type second) {
+template<typename Type>
+Type min(Type first, Type second) {
 	if (first < second)
 		return first;
 	else if (first == second)
@@ -23,7 +25,8 @@ template<typename Type> Type min(Type first, Type second) {
 		return second;
 }
 
-template<typename Type> Type max(Type first, Type second) {
+template<typename Type>
+Type max(Type first, Type second) {
 	if (first > second)
 		return first;
 	else if (first == second)
@@ -32,21 +35,35 @@ template<typename Type> Type max(Type first, Type second) {
 		return second;
 }
 
-// TODO maybe need to check if same type and support comparaison operators
 
 int main(void) {
 	int a = 2;
 	int b = 3;
-
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	char e = 'a';
+	char f = 'b';
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+	float g = 21.2121f;
+	float h = 42.4242f;
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+
 	return 0;
 }
