03fd1414df87a2d92d9e9ee8a631a934b4370534
John Doe <ademenet@student.42.fr>
2017-04-13 16:55:03 +0200

    ex00 done

--- d08/ex00/easyfind.hpp
+++ d08/ex00/easyfind.hpp
@@ -2,17 +2,19 @@
  * @Author: ademenet
  * @Date:   2017-04-13T14:32:36+02:00
  * @Last modified by:   ademenet
- * @Last modified time: 2017-04-13T16:08:02+02:00
+ * @Last modified time: 2017-04-13T16:54:13+02:00
  */
 
 #include <vector>
+#include <list>
+#include <array>
 #include <iostream>
 
 template<typename T>
 int easyfind(T container, int toFind) {
-	for(typename T::iterator ite = container.begin(); ite != container.end(); ite++) { // maybe ++ite
+	for(typename T::iterator ite = container.begin(); ite != container.end(); ite++) {
 		if (*ite == toFind)
 			return *ite;
 	}
-	throw std::exception();
+	throw std::logic_error("didn't find anything");
 }
--- d08/ex00/main.cpp
+++ d08/ex00/main.cpp
@@ -2,7 +2,7 @@
  * @Author: ademenet
  * @Date:   2017-04-13T14:32:36+02:00
  * @Last modified by:   ademenet
- * @Last modified time: 2017-04-13T16:09:37+02:00
+ * @Last modified time: 2017-04-13T16:54:33+02:00
  */
 
 #include "easyfind.hpp"
@@ -11,8 +11,68 @@ int main() {
 	// list
 	// array
 	// int array[4] = { 1, 2, 3, 4 };
-	std::vector<int> container(4, 2);
+	std::cout << "--- Test with vectors" << std::endl;
+	std::vector<int> vec(4);
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(30);
+	vec.push_back(40);
+	try {
+		std::cout << "- Good" << std::endl;
+		std::cout << easyfind< std::vector<int> >(vec, 20) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		std::cout << "- Error" << std::endl;
+		std::cout << easyfind< std::vector<int> >(vec, 2) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 
-	std::cout << easyfind< std::vector<int> >(container, 2) << std::endl;
+	std::cout << "--- Test with list" << std::endl;
+	std::list<int> lst(4);
+	lst.push_back(10);
+	lst.push_back(20);
+	lst.push_back(30);
+	lst.push_back(40);
+	try {
+		std::cout << "- Good" << std::endl;
+		std::cout << easyfind< std::list<int> >(lst, 20) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		std::cout << "- Error" << std::endl;
+		std::cout << easyfind< std::list<int> >(lst, 2) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << "--- Test with array" << std::endl;
+	std::array<int, 4> arr;
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 30;
+	arr[3] = 40;
+	try {
+		std::cout << "- Good" << std::endl;
+		std::cout << easyfind< std::array<int, 4> >(arr, 20) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		std::cout << "- Error" << std::endl;
+		std::cout << easyfind< std::array<int, 4> >(arr, 2) << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
 	return 0;
 }
